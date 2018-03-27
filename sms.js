b6e63b09cc61693f77295d99a4ec8ff87b5d8e70
Michika <mhiratsuka0209@gmail.com>
2018-03-27 03:32:00 -0700

    sms func worked

--- sms.js
+++ sms.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3();
 
 // Twilio Credentials
 const accountSid = 'ACb63cad4d19a7166e9edbf83b7b71ca1d';
-const authToken = 'd1704ca6c77ae12f7fa722bfa6d4160';
+const authToken = 'cd1704ca6c77ae12f7fa722bfa6d4160';
 
 // require the Twilio module and create a REST client
 const client = require('twilio')(accountSid, authToken);
@@ -13,18 +13,20 @@ const client = require('twilio')(accountSid, authToken);
 module.exports.sms = (event) => {
   event.Records.forEach((record) => {
     const filename = record.s3.object.key;
-    const filesize = record.s3.object.size;
-    console.log(`New object has been created: ${filename} (${filesize} bytes)`);
-
-    client.messages.create(
-        {
-          to: '+16047821123',
-          from: '+16042107058',
-          body: 'The new file was added to your S3 bucket',
-        },
-        (err, message) => {
-          console.log(message.sid)
-        }
-    );
-  });
+    console.log(`New object has been created: ${filename}`);
+    sendSms(filename);
+  })
+  
+  function sendSms(filename){
+    let fileName = filename;
+    client.messages.create({
+            to: '+16047821123',
+            from: '+16042107058',
+            body: `The new file ${fileName} was added to your S3 bucket`,
+          },
+          (err, message) => {
+            console.log(message.sid)
+          }
+        )
+  }
 };
